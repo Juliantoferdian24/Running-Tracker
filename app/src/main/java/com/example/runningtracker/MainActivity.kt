0a8c311cf5f113b38a980143228a2b24910b818a
Ferdian Julianto <Juliantoferdian24@gmail.com>
2021-07-14 20:23:52 +0700

    Dagger Hilt Setup

--- app/src/main/java/com/example/runningtracker/MainActivity.kt
+++ app/src/main/java/com/example/runningtracker/MainActivity.kt
@@ -2,8 +2,16 @@ package com.example.runningtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.runningtracker.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/runningtracker/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.runningtracker.util
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
